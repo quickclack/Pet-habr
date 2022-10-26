49e38b584279fbb5cefbda23ad7ca9b48c792894
An-drew-bit <mackarov.makoroff2016@yandex.ru>
2022-10-27 02:58:55 +0300

    add ru localisation in project

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,5 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Auth::routes();
