c21552b0fa66c98b07503cfd2b699e1918f81887
Alexey <72097093+Aleksej-F@users.noreply.github.com>
2022-11-01 00:26:11 +0300

    запрос

--- resources/js/components/Articles/ArticlesList.jsx
+++ resources/js/components/Articles/ArticlesList.jsx
@@ -1,12 +1,23 @@
+import axios from 'axios';
 import React, {useEffect, useState} from 'react';
+import { Await } from 'react-router-dom';
 import Article from './Article';
 function ArticlesList() {
   const [articles, setArticles] = useState([]);
 
 
   useEffect(()=> {
+      getArticles()
+  })
 
-   })
+  const getArticles = async () => {
+    await axios.post("/api/articles")
+      .then(({data})=>{
+        console.log('data', data)
+      }
+
+      )
+  }
    
   return (
       <>
