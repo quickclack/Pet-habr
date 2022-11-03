1f0c1a70e901d27554ad95c6744b9757b5ae8b90
Alexey <72097093+Aleksej-F@users.noreply.github.com>
2022-11-03 21:15:42 +0300

    стили и вывод статей

--- resources/js/components/Articles/Article.jsx
+++ resources/js/components/Articles/Article.jsx
@@ -1,37 +1,22 @@
 import React from 'react';
-import { Link } from "react-router-dom";
-import ArticleStatsIcons from './ArticleStatsIcons.jsx'
-import getArticleDate from '../../hook/articleDate.js'
 
-
-
-function Article({item, key}) {
-  
-  const articleDate = getArticleDate(item.created_at)
-
+function Article({item}) {
+  console.log(item);
   return (
-      <div className="article" >
-        <div className="article__header ">
-          <h4> {item.user.nickName}</h4>
-          <h5> &emsp;{articleDate.dayWriting}&ensp;</h5>
-          <h5> в {articleDate.creationTimeArticle}</h5>
+      <div className="articke">
+        <div className="articke__header d-flex">
+          <h4>Name {item.id}</h4>
+          <h5>в {item.created_at}</h5>
         </div>
-        <div className='article__title'>
-        <Link to={`/article/${item.id}`} className="nav-btn">
-            <h4>{item.title}</h4>
-        </Link>
-          
+        <div className='articke__title'>
+          <h4>{item.title}</h4>
         </div>
-        <div className='article__description'>
+        <div className='articke__description'>
           <p>{item.description}</p>
         </div>
-        <div className='article__button'>
-          <Link to={`/article/${item.id}`} className="nav-btn">
-            Читать далее
-          </Link>
-          
+        <div className='articke__description'>
+          <button>Читать далее</button>
         </div>
-        <ArticleStatsIcons />
       </div>
            
        
--- resources/js/components/Articles/ArticlesList.jsx
+++ resources/js/components/Articles/ArticlesList.jsx
@@ -1,95 +1,43 @@
 import axios from 'axios';
 import React, {useEffect, useState} from 'react';
-import { useSelector, useDispatch } from "react-redux";
-import { Link } from "react-router-dom";
-import { getDbArticlesAll, getArticlesAll, getPaginationLinks, getDbArticlesPage} from "../../store/articles"
+import { Await } from 'react-router-dom';
 import Article from './Article.jsx';
 import './ArticlesList.scss'
 
-
 function ArticlesList() {
-  const dispatch = useDispatch(); 
-  const articles =  useSelector(getArticlesAll);
-  const [currentPage, setCurrentPage] = useState(1)
- 
-  const paginationArray = useSelector(getPaginationLinks).slice(1, -1);
-  // const [articles, setArticles] = useState([]);
+  const [articles, setArticles] = useState([]);
 
-  console.log('pagination', paginationArray)
+  console.log(articles)
 
   useEffect(()=> {
-    console.log("articles dispatch")
-    dispatch( getDbArticlesAll());
+      getArticles()
   },[])
 
-   console.log('articles - ', articles)
+  const getArticles = async () => {
+    await axios.post("/api/articles")
+      .then(({data})=>{
+        console.log('data', data)
+        setArticles(data.articles)
+        console.log(articles)
+        
+      }
 
-  const paginate = (page) =>{
-    
-    let curent = currentPage
-    switch (page) {
-      case '-1' : {
-        if (currentPage > 1) {
-          setCurrentPage( prev => prev - 1)
-          curent--
-        }
-        break;
-      }
-      case '+1' : {
-        if  (currentPage < paginationArray.length) {
-          setCurrentPage( prev => prev + 1)
-          curent++
-        }
-        break;
-      }
-      default:{
-        setCurrentPage(page)
-        curent = page 
-      }
-    }
-    dispatch( getDbArticlesPage(curent));
+      )
+      
   }
    
   return (
-    <>
+      <>
       {
         articles.length > 0 ? articles.map((item, key) =>(
-         <Article key={key} item={item} />
-        )) : <h2>Статей нет</h2>
+         
+          <Article key={key} item={item}/>
+       )) : <h2>Статей нет</h2>
       } 
-      <div className="articleId">
-        <div className="articles__pagination">
-          <div className={`articles__pagination-element `}
-                    onClick = {()=>paginate('-1')}
-            >
-              <span>
-                &laquo; Предыдущая
-              </span>
-          </div>
-          
-          {
-            paginationArray.length > 0 ? paginationArray.map((item, key) =>(
-              <div key = { key } className={`articles__pagination-element ${item.active ? 'active':'' }`}
-                    onClick = {()=>paginate(parseInt(item.label))}
-              >
-                  <span>
-                    {`${item.label}`}
-                  </span>
-              </div>
-            )) : ''
-          }
-
-          <div className={`articles__pagination-element `}
-            onClick = {()=>paginate('+1')}
-          >
-            <span>
-              Следующая &raquo;
-            </span>
-          </div>
-        </div>
-      </div>
-    </>
-  );
-}
+      </>
+           
+       
+    );
+  }
   
 export default ArticlesList;
