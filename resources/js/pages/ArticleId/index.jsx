b6a271b8cd82495525efbaed4baf322b5ae49b8d
Alexey <72097093+Aleksej-F@users.noreply.github.com>
2022-11-06 14:58:59 +0300

    просмотры комментарии рейтинг

--- resources/js/pages/ArticleId/index.jsx
+++ resources/js/pages/ArticleId/index.jsx
@@ -3,53 +3,23 @@ import { useNavigate, useParams, Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { getDbArticle, getArticle } from "../../store/articles"
 import './ArticleId.scss'
-
-
-
-
-
+import ArticleStatsIcons from '../../components/Articles/ArticleStatsIcons.jsx'
+import getArticleDate from '../../hook/articleDate.js'
 
 function ArticleId() {
-  let dayWritingArraySting = {
-    0: "сегодня",
-    1: "вчера",
-    2: "позавчера"
-  }
-  
-  function getDateObject (date) {
-    return { year: date.getFullYear(),
-            month: date.getMonth(),
-            date: date.getDate()
-    }
-  }
+
 
   const dispatch = useDispatch(); 
   const { articleId } = useParams();
   const article = useSelector(getArticle);
   
   console.log('article - ',article)
+  
   useEffect(()=> {
     dispatch( getDbArticle(articleId) );
   },[])
 
-   
-  let dayWriting =''
-  let dateToday = new Date()
-  let dateAtricle = new Date(article.created_at)
-  let dateAtricleObject = getDateObject(dateAtricle)
-  let dateTodayObject = getDateObject(dateToday)
-  let creationTimeArticle = `${dateAtricle.getHours()}:${dateAtricle.getMinutes()}`
-  // console.log('dateAtricle - ',(dateToday - dateAtricle)/(24*3600*1000) )
-  // console.log( dateAtricleObject, dateTodayObject)
-
-  if (dateAtricleObject.year === dateTodayObject.year 
-      && dateAtricleObject.month === dateTodayObject.month
-      && (dateTodayObject.date - dateAtricleObject.date) < 3) {
-     dayWriting = dayWritingArraySting[ dateTodayObject.date - dateAtricleObject.date ]
-  } else dayWriting = article.created_at
-
-
-
+  const articleDate = getArticleDate(article.created_at)
   return (
     <>
       <div className="pages-header">
@@ -59,8 +29,8 @@ function ArticleId() {
       <div className="articleId">
         <div className="articleId__header ">
           <h4> {article.user.nickName}</h4>
-          <h5> &emsp;{dayWriting}&ensp;</h5>
-          <h5> в {creationTimeArticle}</h5>
+          <h5> &emsp;{articleDate.dayWriting}&ensp;</h5>
+          <h5> в {articleDate.creationTimeArticle}</h5>
         </div>
         <div className='articleId__title'>
           <h4>{article.title}</h4>
@@ -68,7 +38,20 @@ function ArticleId() {
         <div className='articleId__description'>
           <p>{article.description}</p>
         </div>
-        
+        <div className='articleId__tags'>
+          <p><span className='articleId__tags-span'>Теги:&ensp;</span>
+            {
+              article.tags.length > 0 ? article.tags.map((item, key) =>(
+                <>{item.title}{key<article.tags.length - 1 ? ',' : '' } </>
+              )) : ''
+            }
+          </p>
+        </div>  
+      </div>
+      
+      
+      <div className="articleId">
+        <ArticleStatsIcons articleId="true"/>
       </div>
     </>
   );
