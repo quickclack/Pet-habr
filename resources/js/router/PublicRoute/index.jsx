228d2939106abfd6bec3fbda2a83e57e0da66117
Alexey <72097093+Aleksej-F@users.noreply.github.com>
2022-11-21 00:56:19 +0300

    настройки в лич каб. sanctum

--- resources/js/router/PublicRoute/index.jsx
+++ resources/js/router/PublicRoute/index.jsx
@@ -4,9 +4,9 @@ import { useSelector } from "react-redux";
 import { getIsAuth } from "../../store/userAuth";
 import All from '../../pages/All';
 
-export const ProtectedRoute = () => {
+export const PublicRoute = () => {
     const isAuthed = useSelector(getIsAuth);
-    return isAuthed ? <All />:<Outlet /> 
+    return isAuthed ? <Outlet /> : <All />
 };
 
  
