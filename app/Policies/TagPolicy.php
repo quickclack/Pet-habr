cff721e947e725c5dad90069f5e9713bda499aff
An-drew-bit <mackarov.makoroff2016@yandex.ru>
2022-12-15 17:28:08 +0300

    add policy in admin panel

---
+++ app/Policies/TagPolicy.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Policies;
+
+use Domain\User\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+use Illuminate\Auth\Access\Response;
+
+class TagPolicy
+{
+    use HandlesAuthorization;
+
+    public function viewAny(): bool
+    {
+        return true;
+    }
+
+    public function view(): bool
+    {
+        return true;
+    }
+
+    public function create(User $user): Response
+    {
+        return $user->roles->containsStrict('id', 1)
+            ? Response::allow()
+            : Response::deny();
+    }
+
+    public function update(User $user): Response
+    {
+        return $user->roles->containsStrict('id', 1)
+            ? Response::allow()
+            : Response::deny();
+    }
+
+    public function delete(User $user): bool
+    {
+        return $user->roles->containsStrict('id', 1);
+    }
+}
